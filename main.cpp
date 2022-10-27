8ad1aa0e49c5d2a4edc17c2a85a1a2903676f86d
Pau Fernandez Bardulet <pau.fernandezb@estudiants.urv.cat>
2022-10-27 18:56:13 +0200

    Added LED and buzzer functionality

--- main.cpp
+++ main.cpp
@@ -7,16 +7,30 @@ int main()
     Photoresistor photoresistor(A0);
     AnalogIn potenciometer(A1);
     Grove_LCD_RGB_Backlight screen(D14, D15);
+    PwmOut led(D3);
+    PwmOut buzzer(D5);
+
+    buzzer.period(0.01);
+
     while (true) {
         float lux_percent = photoresistor.read_percent();
         float pot_percent = potenciometer.read();
         printf("Lux: %f%%, Pot: %f%%\n", lux_percent * 100, pot_percent * 100);
 
         float lux_compensated_percent;
-        if (lux_percent < pot_percent)
-        {
+        if (lux_percent <= pot_percent)
             lux_compensated_percent = pot_percent - lux_percent;
-        }
+        else
+            lux_compensated_percent = 0.0;
+        
+        if (lux_percent < 0.0)
+            buzzer.write(0.25);
+        else
+            buzzer.write(0.0);
+
+        led.write(lux_compensated_percent);
+
+        screen.setRGB(58, 52, 235);
 
         char lux_str[16] = "lux: ";
         char lux_percent_str[16];
